bfa82f7d326d6026d1ac442548ed4ef4e8cb1662
rese <garcia.moreno.ruben@gmail.com>
2018-01-27 13:30:10 +1100

    Added particle explosion.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject enemyExplosionPrefab;
+
     public float speed;
     public MeshRenderer renderer;
     public WaveColour colour;
@@ -19,9 +22,15 @@ public class Enemy : MonoBehaviour
     {
         transform.position -= Vector3.up * speed * Time.deltaTime;
 	}
-
+    
     public void Kill ()
     {
+        ParticleSystemRenderer explosion = GameObject.Instantiate(enemyExplosionPrefab).GetComponent<ParticleSystemRenderer>();
+        explosion.transform.position = transform.position;
+
+        explosion.material.SetColor("_Color", GameUtil.GetColor(colour));
+        explosion.material.SetColor("_TintColor", GameUtil.GetColor(colour));
+
         GameObject.Destroy(gameObject);
     }
 }
